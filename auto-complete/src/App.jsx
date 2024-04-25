e195bb2b38e45362e914ef0e93c1afc604e6246f
Dwij13 <dwijjindal2005@gmail.com>
2024-04-25 12:17:55 +0530

    Complete

---
+++ auto-complete/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; 
+import Search from './Components/Search';
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Search</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
